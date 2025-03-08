e88a9668f75c1a163b4f630518e4e6dcff4239b7
gutembergsa <gutemberg.silvasa@outlook.com>
2025-03-08 12:28:13 -0300

    remove ts warnings

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { patchCssModules } from 'vite-css-modules'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    patchCssModules(), // ← This is all you need to add!
+  ],
 })
