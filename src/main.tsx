d21a9a037d5912d53e969fd5f15580cb9d55b545
gutembergsa <gutemberg.silvasa@outlook.com>
2025-03-14 10:12:55 -0300

    trabalhando no SEO 1

--- src/main.tsx
+++ src/main.tsx
@@ -38,6 +38,10 @@ createRoot(document.getElementById('root')!).render(
       <Helmet>
         <title>olhAI</title>
         <meta
+          name="google-site-verification"
+          content="yvvFmGjsP6baaYBQWZbjaQLzzs-FUMEVC85FUSk0ivU"
+        />
+        <meta
           name="description"
           content="Sistemas de visão para seu processo."
         />
